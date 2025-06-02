7f5f6ec483558801ba8097a3f83a2be4ee00efbf
Georgina <georginatomass@gmail.com>
2025-06-02 22:45:31 +0200

    Add projects data and enhance animations in CSS files

---
+++ assets/data/projects.js
@@ -0,0 +1,18 @@
+const projectsList = [
+    {
+        id: 1,
+        name: "Mels del Montnegre",
+        description: "A brief description of Project A.",   
+        technologies: ["HTML", "CSS", "JavaScript"],
+        link: "https://example.com/project-a",
+        github: "https://github.com/username/project-a"
+    },
+    {
+        id: 2,
+        name: "TodoApp",
+        description: "A brief description of Project B.",
+        technologies: ["Vue.js","Tailwind", "Node.js", "Express"],
+        link: "https://example.com/project-b",
+        github: "https://github.com/username/project-b"             
+    }
+]
